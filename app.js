f6be9f9b0d1d2fcde9d1e0dd797b4833575d96c9
RobRusu <142660890+RobRusu@users.noreply.github.com>
2024-06-13 12:16:22 +0100

    added set function that adds key-value pairs to the hashmap

--- app.js
+++ app.js
@@ -1,2 +1,48 @@
 #!/usr/bin/env node
 
+import { LinkedList } from "./linkedList.js";
+
+// Use the following snippet whenever you access a bucket through an index. We want to throw an error if we try to access an out of bound index:
+
+// if (index < 0 || index >= buckets.length) {
+//   throw new Error("Trying to access index out of bound");
+// }
+
+class HashMap {
+  constructor(size = 16) {
+    this.size = size;
+    this.hashMap = new Array(this.size).fill(null).map(() => new LinkedList());
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.size;
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {
+    let index = this.hash(key);
+    let buckets = this.hashMap;
+
+    for (let i = 0; i < buckets[index].size(); i++) {
+      if (buckets[index].at(i + 1).value.key === key) {
+        buckets[index].at(i + 1).value.value = value;
+        return;
+      }
+    }
+    buckets[index].append({ key, value });
+  }
+}
+
+const map = new HashMap();
+
+map.set("Carlos", "I am the old value");
+map.set("Carlos", "I am the new value");
+map.set("Kim", "I am the old value");
+map.set("Sam", "I am the newest value");
+console.log(map.hashMap);
---
+++ linkedList.js
@@ -0,0 +1,175 @@
+export class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    if (this.head === null) {
+      //if head is empty add node
+      this.head = new Node(value);
+    } else {
+      //otherwise add value to the end of list
+      let head = this.head;
+      while (head.nextNode !== null) {
+        head = head.nextNode;
+      }
+      head.nextNode = new Node(value);
+    }
+  }
+
+  prepend(value) {
+    if (this.head === null) {
+      //if head is empty add node
+      this.head = new Node(value);
+    } else {
+      //otherwise add value to the start of the list and make it new head
+      let newHead = new Node(value);
+      newHead.nextNode = this.head;
+      this.head = newHead;
+    }
+  }
+
+  size() {
+    if (this.head === null) {
+      //if head is empty return 0
+      return "0 nodes";
+    } else {
+      //otherwise return total number of nodes
+      let head = this.head;
+      let nodes = 1;
+      while (head.nextNode !== null) {
+        head = head.nextNode;
+        nodes++;
+      }
+      return nodes;
+    }
+  }
+
+  headNode() {
+    if (this.head === null) {
+      //if head is empty return 0
+      return 0;
+    } else {
+      //otherwise return head node
+      return this.head;
+    }
+  }
+
+  tailNode() {
+    if (this.head === null) {
+      //if head is empty return 0
+      return 0;
+    } else {
+      //otherwise return last node
+      let tail = this.head;
+      while (tail.nextNode !== null) {
+        tail = tail.nextNode;
+      }
+      return tail;
+    }
+  }
+
+  at(index) {
+    if (this.head === null || index <= 0 || !index) {
+      //if head is empty or input is invalid return message
+      return "List is empty";
+    } else {
+      //otherwise return node at index
+      let indexNode = this.head;
+      let nodes = 0;
+      while (indexNode.nextNode !== null) {
+        nodes++;
+        if (nodes == index) {
+          return indexNode;
+        }
+        indexNode = indexNode.nextNode;
+      }
+      // return last node
+      if (index <= nodes + 1) return indexNode;
+      if (index > nodes) return "Out of range";
+    }
+  }
+
+  pop() {
+    if (this.head === null) {
+      //if head is empty return 0
+      return 0;
+    } else {
+      //otherwise loop until end of node and remove penultimate next node reference
+      let indexNode = this.head;
+      let previousNode;
+      while (indexNode.nextNode !== null) {
+        previousNode = indexNode;
+        indexNode = indexNode.nextNode;
+      }
+      previousNode.nextNode = null;
+    }
+  }
+
+  contains(value) {
+    let headNode = this.head;
+    if (this.head === null) {
+      //if head is empty return false
+      return false;
+    } else {
+      //otherwise loop and check if value is in the list
+      while (headNode.nextNode !== null) {
+        if (headNode.value === value) {
+          return true;
+        } else {
+          headNode = headNode.nextNode;
+        }
+      }
+    }
+    if (headNode.value === value) {
+      //if last node contains the value return true
+      return true;
+    }
+    //otherwise return false
+    return false;
+  }
+
+  find(value) {
+    let indexNode = this.head;
+    let nodes = 0;
+
+    while (indexNode.nextNode !== null) {
+      //loop through the list until value is found
+      if (indexNode.value === value) {
+        return nodes;
+      }
+      nodes++;
+      indexNode = indexNode.nextNode;
+    }
+    if (indexNode.value === value) {
+      //check last node for value
+      return nodes;
+    }
+    //if value isn't found return null
+    return null;
+  }
+
+  toString() {
+    let listString = "";
+    let node = this.head;
+
+    //if head is empty return message
+    if (node === null) {
+      return "List is empty.";
+    }
+
+    while (node.nextNode !== null) {
+      //loop through list and update listString
+      listString = `${listString}( ${node.value} ) -> `;
+      node = node.nextNode;
+    }
+    //return listString, last node value and null
+    return `${listString} ( ${node.value} ) -> ${node.nextNode}`;
+  }
+}
+
+class Node {
+  constructor(value = null, nextNode = null) {
+    (this.value = value), (this.nextNode = nextNode);
+  }
+}
